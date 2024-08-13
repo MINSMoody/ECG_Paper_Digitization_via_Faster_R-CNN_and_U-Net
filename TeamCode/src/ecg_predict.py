834390bd7736c54e18ae57da3d75be728096568f
Joseph <hshang@ethz.ch>
2024-08-13 17:11:15 +0200

    unet snr 6.303

--- TeamCode/src/ecg_predict.py
+++ TeamCode/src/ecg_predict.py
@@ -9,6 +9,7 @@ from TeamCode.src.ecg_models import build_model
 from imageio import imread, imwrite
 # from torch.autograd import Variable
 from concurrent.futures import ThreadPoolExecutor
+import pickle
 
 
 class ECGPredictor(object):
@@ -42,22 +43,27 @@ class ECGPredictor(object):
         return prednp
     
     
-    def run(self, images, bboxes):
+    def run(self, image, bboxes):
         self.res_maps = [None] * len(bboxes)  # Initialize with None to preserve order
-        
+        if image.ndim == 3:  # Image has 3 dimensions and the third dimension has 3 channels
+            # Convert RGBA to grayscale
+            image = np.dot(image[..., :3], [0.2989, 0.5870, 0.1140])
         def process_bbox(index, image, bbox):
             return index, self.__run(image, bbox)
         
         with ThreadPoolExecutor() as executor:
             # Submit tasks with their indices
-            futures = {executor.submit(process_bbox, i, images[i], bbox): i for i, bbox in enumerate(bboxes)}
+            futures = {executor.submit(process_bbox, i, image, bbox): i for i, bbox in enumerate(bboxes)}
             
             for future in futures:
                 index, res_map = future.result()  # Get index and result from the future
                 self.res_maps[index] = res_map  # Place result in the correct index
-        assert len(self.res_maps) == len(images), f"Length of res_maps and images do not match: {len(self.res_maps)} != {len(images)}"
+        # assert len(self.res_maps) == len(images), f"Length of res_maps and images do not match: {len(self.res_maps)} != {len(images)}"
+        #save the res_maps as a pickle file
+        with open('images.pkl', 'wb') as f:
+            pickle.dump(self.res_maps, f)
         # pad the res_maps with 0s to match the size of the images and convert to numpy array
-        masks = np.zeros_like(images)
+        masks = np.zeros((13, image.shape[0], image.shape[1]))
         for i, res_map in enumerate(self.res_maps):
             x1, y1, x2, y2 = bboxes[i]
             masks[i, y1:y2, x1:x2] = res_map
@@ -65,10 +71,12 @@ class ECGPredictor(object):
         
         # res_maps = torch.tensor(self.res_maps)
         # assert res_maps.shape == images.shape, f"Shapes of res_maps and images do not match: {res_maps.shape} != {images.shape}"
-        return masks, bboxes
+        return masks
 
 
     def __run(self, image, bbox=None):
+        if(image.ndim == 3):
+            image = np.dot(image[..., :3], [0.2989, 0.5870, 0.1140])
         assert image.ndim == 2, "Image should be grayscale, but has shape: {}".format(image.shape)
         if bbox is not None:
             x1, y1, x2, y2 = bbox
@@ -103,6 +111,9 @@ class ECGPredictor(object):
             
         # print(f"Prediction Stats - min: {res_map.min()}, max: {res_map.max()}, mean: {res_map.mean()}")
         # print(f"Prediction Stats - min: {count_map.min()}, max: {count_map.max()}, mean: {count_map.mean()}")
+        
+        # replace the zero elements in count_map with 1 to avoid division by zero
+        count_map[count_map == 0] = 1
         res_map = res_map / count_map
         res_map = res_map[p:-p, p:-p]
         return res_map
